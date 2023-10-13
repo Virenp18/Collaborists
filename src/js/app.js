6c0e8042b54513ed4ea8ec4695326414a16c3b56
Malik-Ankita <2021ankita21@gmail.com>
2023-10-13 12:19:51 -0400

    added create board button

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();   
 });
+    var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
+    var dropdownList = dropdownElementList.map(function (dropdownToggle) {
+        return new bootstrap.Dropdown(dropdownToggle);
+    });
+    
